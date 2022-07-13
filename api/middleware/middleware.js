f4077140ebc5627c5eb8681171dc784e6e4d4e48
Joseph Watkins <josephxwatkins@gmail.com>
2022-07-13 15:49:47 -0400

    router setup commit issue resolved

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,13 +1,34 @@
+const Users = require('../users/users-model');
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  //still needs time stamp added to the log
+  console.log(req.method, req.originalUrl);
+  next();
 }
 
-function validateUserId(req, res, next) {
+ async function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  let id = req.params.id;
+  let user = await Users.getById(id)
+  if (!user) {
+    res.status(404).json({ message: "user not found" });
+    return;
+  }
+  req.user = user
+  next();
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  if (typeof req.body.name !== 'string' || req.body.name.trim() === '') {
+    next({ message: 'missing required name field', status: 400 });
+    return;
+  }
+  req.newUser = {
+      name: req.body.name.trim(),
+  };
+  next();
 }
 
 function validatePost(req, res, next) {
@@ -15,3 +36,9 @@ function validatePost(req, res, next) {
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+};
--- api/server.js
+++ api/server.js
@@ -2,12 +2,29 @@ const express = require('express');
 
 const server = express();
 
+const usersRouter = require('./users/users-router');
+
+const { logger } = require('./middleware/middleware');
+
 // remember express by default cannot parse JSON in request bodies
+server.use(express.json());
+
+server.use('/api/users', usersRouter);
 
 // global middlewares and the user's router need to be connected here
+server.use(logger)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
+
+server.use((error, req, res, next) => {
+  console.log(error);
+  res.status(error.status || 500).json({
+    message: error.message || 'internal server error'
+  });
+});
+
+//commit issue////ongoing...
 
 module.exports = server;
